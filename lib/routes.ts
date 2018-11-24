898795a1a6d68a1a4412a7f7eefe2f40eaebe889
Marek Mitis <mmitis@grupa.onet>
2018-11-24 15:28:37 +0100

    GET

--- lib/routes.ts
+++ lib/routes.ts
@@ -3,10 +3,30 @@ import {Request, Response, NextFunction} from "express";
 export class Routes {
 
     public routes(app): void {
+
+
+        app.route('/bets')
+            .get((req: Request, res: Response) => {
+                const response = {
+                    progress: {
+                        country: 0.1,
+                        voivodeship: 0.3
+                    },
+                    bets: app.db.bets,
+                    results: app.db.results,
+                    user : req.params.userId ? app.db.users.find((user : User) => user.userId === req.params.userId) : null
+                };
+                res.status(200).json(response);
+            })
+            .post((req: Request, res: Response, next: NextFunction) => {
+                res.status(200).json({});
+            });
+
+
         app.route('/')
             .get((req: Request, res: Response) => {
                 res.status(200).json({
-                    message: 'GET request successfulll!!!!'
+                    message: 'API works'
                 })
             })
     }
