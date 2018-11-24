77663a648f37776ce5de406eea567573cb6288b0
Marek Mitis <mmitis@grupa.onet>
2018-11-24 15:05:16 +0100

    heroku

--- lib/routes.ts
+++ lib/routes.ts
@@ -3,13 +3,11 @@ import {Request, Response, NextFunction} from "express";
 export class Routes {
 
     public routes(app): void {
-
         app.route('/')
             .get((req: Request, res: Response) => {
                 res.status(200).json({
                     message: 'GET request successfulll!!!!'
                 })
             })
-
     }
 }
