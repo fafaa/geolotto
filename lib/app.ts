c672e9aa849f5454865d9967ea890ec4b4d6c06d
Marek Mitis <mmitis@grupa.onet>
2018-11-24 17:37:58 +0100

    Cors

--- lib/app.ts
+++ lib/app.ts
@@ -1,5 +1,7 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import * as cors from "cors";
+
 import {Routes} from "./routes";
 import {CONFIG} from "./config";
 import {Database} from "./interfaces/Database";
@@ -47,6 +49,7 @@ class App {
     }
 
     private configApp(): void{
+        this.app.use(cors());
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
         // serving static files
